6db0cf7e54051443d28621193782b42be384d7de
mteatran <tran.malina@gmail.com>
2015-12-06 20:19:45 -0500

    Assets

--- breatherapp/app/controllers/miscellaneous_controller.rb
+++ breatherapp/app/controllers/miscellaneous_controller.rb
@@ -3,6 +3,10 @@ class MiscellaneousController < ApplicationController
   skip_before_action :authenticate_user!, only: :index
 
   def index
+    @home_page = true
+    if current_user
+      redirect_to '/dashboard'
+    end
   end
 
   def dashboard
--- breatherapp/config/routes.rb
+++ breatherapp/config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-  root 'miscellaneous#dashboard'
+  root 'miscellaneous#index'
+  get '/dashboard' => 'miscellaneous#dashboard'
   get '/connect' => 'connect#index'
   post '/sendtext' => 'send_text#create'
 
